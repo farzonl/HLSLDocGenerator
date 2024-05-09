1e345431048c5586e0810f163e9be745da0a5bb3
Farzon Lotfi <farzonlotfi@microsoft.com>
2024-05-09 13:24:25 -0400

    update, regex, add more range enforcement for git commits

--- llvm_git_graph.py
+++ llvm_git_graph.py
@@ -53,7 +53,7 @@ def getopNames(opcodes):
     return opcodeNames
 
 def extract_all_opcodes(text):
-    matches = re.findall(r'def \w+ : DXILOpMapping<\s*(\d+)', text)
+    matches = re.findall(r'def\s+\w+\s*:\s*DXILOpMapping<\s*(\d+)', text)
     if matches:
        return matches
     return []
@@ -77,9 +77,9 @@ def extract_all_hlsl_intrinsics(text):
 def llvm_graph_hlsl_intrinsics(data=None):
     if not data:
         data =  llvm_git_hlsl_parser()
-    dates = [date for date, _, _, _ in data]
-    intrinsics = [ int_count for _, int_count, _, _ in data]
-    percentages = [ percentage for _, _, percentage, _ in data]
+    dates = [date for date, _, _, _, _ in data]
+    intrinsics = [ int_count for _, int_count, _, _, _ in data]
+    percentages = [ percentage for _, _, percentage, _, _ in data]
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=dates, y=intrinsics, mode='lines', name='HLSL intrinsics'))
     fig.add_trace(go.Scatter(x=dates, y=percentages, mode='lines', name='HLSL int %'))
@@ -102,15 +102,15 @@ def llvm_git_hlsl_parser():
     hlsl_intrinsic_files = get_git_commits(llvm_path, hlsl_intrinsic_rel_path)
   
     data = [
-    ['Date',  '# of Intrinsics', f"% of Intrinsics", "Intrinsics"]
+    ['Date',  '# of Intrinsics', f"% of Intrinsics", "Intrinsics", "commit id"]
     ]
     intrinsics = []
     num_intrinsics = 0
-    for date, file_version in hlsl_intrinsic_files:
+    for date, file_version, git_sha in hlsl_intrinsic_files:
         intrinsics = extract_all_hlsl_intrinsics(file_version)
         num_intrinsics = len(intrinsics)
         if num_intrinsics > 0:
-            row = [date.strftime('%Y-%m-%d'), num_intrinsics, round(100 * (num_intrinsics / len(all_hlsl_intrinsic)),2), ', '.join(intrinsics)]
+            row = [date.strftime('%Y-%m-%d'), num_intrinsics, round(100 * (num_intrinsics / len(all_hlsl_intrinsic)),2), ', '.join(intrinsics), git_sha]
             data.append(row)
             print_cli(f"Date: {row[0]} intrinsic count: {num_intrinsics}")
     
@@ -125,15 +125,15 @@ def llvm_git_dxil_parser():
     dxil_opcode_files = get_git_commits(llvm_path, dxil_opcode_rel_path)
 
     data = [
-    ['Date',  '# of DXILOps', f"% of DXILOps", "DXILOp ids", "DXILOp Names"]
+    ['Date',  '# of DXILOps', f"% of DXILOps", "DXILOp ids", "DXILOp Names", "commit id"]
     ]
     opcodes = []
     num_opcodes = 0
-    for date, file_version in dxil_opcode_files:
+    for date, file_version, git_sha in dxil_opcode_files:
         opcodes = extract_all_opcodes(file_version)
         num_opcodes = len(opcodes)
         if num_opcodes > 0:
-            row = [date.strftime('%Y-%m-%d'), num_opcodes, round(100 * (num_opcodes / len(all_dxil_ops)),2), ', '.join(opcodes), ', '.join(getopNames(opcodes))]
+            row = [date.strftime('%Y-%m-%d'), num_opcodes, round(100 * (num_opcodes / len(all_dxil_ops)),2), ', '.join(opcodes), ', '.join(getopNames(opcodes)), git_sha]
             data.append(row)
             print_cli(f"Date: {row[0]} opcode count: {num_opcodes}")
     
@@ -143,9 +143,9 @@ def llvm_git_dxil_parser():
 def llvm_graph_dxil_intrinsics(data=None):
     if not data:
         data =  llvm_git_dxil_parser()
-    dates = [date for date, _, _, _, _ in data]
-    dxilops = [ dxil_count for _, dxil_count, _, _, _ in data]
-    percentages = [  int_per for _, _,  int_per, _, _ in data]
+    dates = [date for date, _, _, _, _, _ in data]
+    dxilops = [ dxil_count for _, dxil_count, _, _, _, _ in data]
+    percentages = [  int_per for _, _,  int_per, _, _, _ in data]
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=dates, y=dxilops, mode='lines', name='DXIL Ops'))
     fig.add_trace(go.Scatter(x=dates, y=percentages, mode='lines', name='DXIL Ops %'))
@@ -160,6 +160,11 @@ def llvm_graph_dxil_intrinsics(data=None):
     else:
         fig.write_image("scratch/dxil_fig.png")
     return fig
+
+def enforce_date_range(committed_datetime, current_date):
+    if current_date >= committed_datetime:
+        return abs(committed_datetime - current_date)
+    return  timedelta.max
 
 def get_git_commits(repo_path: str, file_path : str):
     repo = git.Repo(repo_path)
@@ -176,14 +181,14 @@ def get_git_commits(repo_path: str, file_path : str):
 
     file_versions = []
     current_date = first_commit_date
-    while current_date <= last_commit_date:
-        closest_commit = min(commits, key=lambda x: abs(x.committed_datetime - current_date))
+    while current_date <= (last_commit_date + week_delta):
+        closest_commit = min(commits, key=lambda x: enforce_date_range(x.committed_datetime, current_date))
     
         # Get the version of the file in the closest commit
         version = closest_commit.tree[file_path].data_stream.read().decode('utf-8')
-        file_versions.append((current_date, version))
+        file_versions.append((current_date, version, closest_commit.hexsha))
     
         current_date += week_delta
     return file_versions
 
-llvm_graph_hlsl_intrinsics()
+#llvm_git_hlsl_parser()
